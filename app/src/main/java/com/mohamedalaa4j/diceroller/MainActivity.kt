26628ad994253ef6a7ebb588923ffbddb38411d9
mohamedalaa4j <mohamedalaa4j@gmail.com>
2022-12-04 00:58:41 +0200

    22. Exercise: Adding the ImageView done + getString used

--- app/src/main/java/com/mohamedalaa4j/diceroller/MainActivity.kt
+++ app/src/main/java/com/mohamedalaa4j/diceroller/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        binding?.btnRoll?.text = "Let's Roll"
+        binding?.btnRoll?.text = getString(R.string.Lets_Roll)
 
         binding?.btnRoll?.setOnClickListener {
             rollDice()
@@ -30,6 +30,15 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
 
         val randomNumber = Random().nextInt(6) + 1
-        binding?.tvResult?.text = randomNumber.toString()
+        val diceImage = when(randomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {R.drawable.dice_6}
+        }
+
+        binding?.ivDice?.setImageResource(diceImage)
     }
 }
