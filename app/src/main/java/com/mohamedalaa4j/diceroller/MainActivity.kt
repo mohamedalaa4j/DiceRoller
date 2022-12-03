672c900106e1ddde723e7ed36ba677e4c3252a93
mohamedalaa4j <mohamedalaa4j@gmail.com>
2022-12-03 23:39:59 +0200

    17. Exercise: findViewById (in viewBinding way) done

--- app/src/main/java/com/mohamedalaa4j/diceroller/MainActivity.kt
+++ app/src/main/java/com/mohamedalaa4j/diceroller/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mohamedalaa4j.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mohamedalaa4j.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnRoll?.text = "Let's Roll"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        //A clean way to avoid memory leaks
+        binding = null
     }
 }
